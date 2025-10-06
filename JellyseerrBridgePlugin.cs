bdb3f4c7d6ce7de2109319af5134f15b2d97c7dd
kinggeorges12 <kinggeorges1212@gmail.com>
2025-10-05 21:32:04 -0400

    Release v0.19: Force version update with aggressive changes

--- JellyseerrBridgePlugin.cs
+++ JellyseerrBridgePlugin.cs
@@ -16,7 +16,7 @@ namespace Jellyfin.Plugin.JellyseerrBridge
         public override Guid Id => new Guid("8ecc808c-d6e9-432f-9219-b638fbfb37e6");
         public override string Name => "Jellyseerr Bridge";
         
-        public new string Version => "0.18.0.0";
+        public new string Version => "0.19.0.0";
         
         public string? Image => null;
         
