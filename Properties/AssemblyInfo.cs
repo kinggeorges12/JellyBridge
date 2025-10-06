2a7570f775c22489e88833543052d5fbe05c609e
kinggeorges12 <kinggeorges1212@gmail.com>
2025-10-05 23:15:25 -0400

    Release v0.27: CRITICAL FIX - Fixed GUID mismatch between AssemblyInfo.cs and plugin class

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8ecc808c-d6e9-432f-9219-b638fbfb37e6")]
+[assembly: Guid("088e8efc-6855-42ec-bcc9-24fde6da7149")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.26.0.0")]
-[assembly: AssemblyFileVersion("0.26.0.0")]
-[assembly: AssemblyInformationalVersion("0.26.0.0")]
+[assembly: AssemblyVersion("0.27.0.0")]
+[assembly: AssemblyFileVersion("0.27.0.0")]
+[assembly: AssemblyInformationalVersion("0.27.0.0")]
