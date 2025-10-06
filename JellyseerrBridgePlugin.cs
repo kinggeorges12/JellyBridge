c9eb8289d1a700b8048ac8db4816f869a0f99f8b
kinggeorges12 <kinggeorges1212@gmail.com>
2025-10-05 21:50:31 -0400

    Update all version numbers to 0.21.0.0

--- JellyseerrBridgePlugin.cs
+++ JellyseerrBridgePlugin.cs
@@ -14,9 +14,9 @@ namespace Jellyfin.Plugin.JellyseerrBridge
     public class JellyseerrBridgePlugin : BasePlugin<PluginConfiguration>, IHasPluginConfiguration, IHasWebPages
     {
         public override Guid Id => new Guid("8ecc808c-d6e9-432f-9219-b638fbfb37e6");
-        public override string Name => "Jellyseerr Bridge v0.20";
+        public override string Name => "Jellyseerr Bridge v0.21";
         
-        public new string Version => "0.20.0.0";
+        public new string Version => "0.21.0.0";
         
         public string? Image => null;
         
